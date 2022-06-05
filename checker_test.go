eca808542ae3ddf30b9752a0a775b09f6e525804
Thifnmi <tuthin2k@gmail.com>
2022-06-06 00:28:23 +0700

    as

---
+++ checker_test.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"bytes"
+	"errors"
+
+	"gopkg.in/check.v1"
+)
+
+type CheckerSuite struct{}
+
+var _ = check.Suite(&CheckerSuite{})
+
+func (s *CheckerSuite) TestCheckExecuteCheckerFunc(c *check.C) {
+	err := Check([]Checker{
+		func() error { return errors.New("Fake checker error") },
+	})
+	c.Assert(err, check.NotNil)
+	c.Assert(err.Error(), check.Equals, "Fake checker error")
+}
+
+func (s *CheckerSuite) TestCheckConsideringWarningsAsErrors(c *check.C) {
+	err := Check([]Checker{
+		func() error { return NewWarning("my warning") },
+	})
+	c.Assert(err, check.NotNil)
+	c.Assert(err.Error(), check.Equals, "my warning")
+}
+
+func (s *CheckerSuite) TestCheckWithWarnings(c *check.C) {
+	buf := bytes.NewBuffer(nil)
+	err := CheckWithWarnings([]Checker{
+		func() error { return NewWarning("my warning") },
+	}, buf)
+	c.Assert(err, check.IsNil)
+	c.Assert(buf.String(), check.Equals, "WARNING: my warning\n")
+}
